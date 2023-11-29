3fbc5dd0d1dd9bbe37d176aff14640795d2785dd
koturn <jeak.koutan.apple@gmail.com>
2023-11-29 15:46:22 +0900

    refactor: Move some methods about hot key to HotKeyManader

---
+++ MouseShaker/WindowMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace MouseShaker
+{
+    /// <summary>
+    /// Window message ID constants.
+    /// </summary>
+    public enum WindowMessages : int
+    {
+        /// <summary>
+        /// Windows message ID about Hot Keys.
+        /// </summary>
+        HotKey = 0x0312
+    }
+}
